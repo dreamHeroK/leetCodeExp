cbe3979e0871b571c9892cab4952351c7a95a649
Leo Kang <leo.kang@bridgelegal.biz>
2026-06-04 17:02:39 +0800

    feat: add implementations for valid parentheses and digit sum problems

---
+++ doublePoint/20.有效的括号.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=java
+ *
+ * [20] 有效的括号
+ */
+
+// @lc code=start
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') stack.push(')');
+            else if (c == '[') stack.push(']');
+            else if (c == '{') stack.push('}');
+            else {
+                if (stack.isEmpty() || stack.pop() != c) return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
+// @lc code=end
---
+++ doublePoint/258.各位相加.java
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=258 lang=java
+ *
+ * [258] 各位相加
+ */
+
+// @lc code=start
+class Solution {
+    public int addDigits(int num) {
+        if (num == 0) {
+            return 0;
+        }
+        return 1 + (num - 1) % 9;
+    }
+}
+// @lc code=end
