20457ee789fc8ea466c18ee9ea2ab60f9003c1ca
Leo Kang <leo.kang@bridgelegal.biz>
2026-06-08 14:53:00 +0800

    feat: update VSCode settings and add implementation for two sum problem

---
+++ javaExp/1.两数之和.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=java
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {};
+    }
+}
+// @lc code=end
